7b510931cb6cf9a4bee0e83cee42b678108a9c9c
Elvin Ezizli <elvin@example.com>
2023-04-02 20:16:55 +0400

    .

---
+++ ClassMetod/Program.cs
@@ -0,0 +1,55 @@
+﻿using ClassMetod;
+using System.Reflection.Emit;
+
+NewMethod();
+
+static void NewMethod()
+{
+
+    Student student = new Student("ab","bc",12);
+
+    int std = 1;
+    for (int i = 0; i < std; i++)
+    {
+        std++;
+
+        
+        {
+            string click = Console.ReadLine();
+            if (click == "1")
+            {
+
+                Console.WriteLine("Created Student"); ;
+                Console.Write("Telebenin adi -" + " ");
+                student.Name = Console.ReadLine();
+                Console.Write("Telebenin soyadi -"+" ");
+                student.Surname = Console.ReadLine();
+                Console.Write("telebenin Id-si -" + " ");
+                Int32.TryParse(Console.ReadLine(), out student.Id);
+               
+            }
+           
+            
+            
+            if (click == "2")
+            {
+
+               Console.WriteLine("Get Studnet");
+               Console.WriteLine(student.Name + " " + student.Surname );
+            }
+            
+
+            
+            if(click == "3") 
+            {
+                Console.WriteLine("Exit");
+                break;
+            }
+        }
+
+      
+       
+      
+    }
+   
+}
---
+++ ClassMetod/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ClassMetod
+{
+    internal class Student
+    {
+
+        public string Name;
+        public string Surname;
+        public int Id;
+        public Student(string Name,string Surname,int Id)
+        {
+
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Id = Id;
+        }
+    }
+        
+}
